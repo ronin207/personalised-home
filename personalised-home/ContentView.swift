b301b9293cf2cf36d39e5fa75e8aa43a2757ac69
ronin207 <takumi.ot0911@gmail.com>
2023-11-27 11:39:48 +0900

    Initial Commit

---
+++ personalised-home/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  personalised-home
+//
+//  Created by Takumi Otsuka on 2023/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ personalised-home/personalised_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalised_homeApp.swift
+//  personalised-home
+//
+//  Created by Takumi Otsuka on 2023/11/27.
+//
+
+import SwiftUI
+
+@main
+struct personalised_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
